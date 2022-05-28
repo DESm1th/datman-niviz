aa9ef04e112ac4085b6726b59d9f74b90d4f5099
Dawn Smith <dawn.smith@camh.ca>
2022-05-27 20:26:16 -0400

    [FIX] Add the database binds from the config file

--- __init__.py
+++ __init__.py
@@ -5,10 +5,10 @@ from flask import Blueprint
 from .utils import get_config
 
 niviz_bp = Blueprint(
-    'niviz_rater',
+    "niviz_rater",
     __name__,
-    template_folder='templates',
-    url_prefix='/study/<string:study>/pipeline/<string:pipeline>'
+    template_folder="templates",
+    url_prefix="/study/<string:study>/pipeline/<string:pipeline>"
 )
 
 
@@ -19,7 +19,11 @@ def register_bp(app):
                      "Configuration not found.")
         return
     app.register_blueprint(niviz_bp)
-    app.config['NIVIZ_RATER_CONF'] = config
+    app.config["NIVIZ_RATER_CONF"] = config
+
+    base_uri = app.config["DATABASE_ROOT_URI"]
+    for db_name in config:
+        app.config["SQLALCHEMY_BINDS"][db_name] = f"{base_uri}/{db_name}"
     return app
 
 
