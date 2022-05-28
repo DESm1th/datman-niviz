765d6409163dae281abf57c4f465e267b7a2fb0d
Dawn Smith <dawn.smith@camh.ca>
2022-05-27 20:22:15 -0400

    [FIX] Implement register_bp

--- __init__.py
+++ __init__.py
@@ -1,4 +1,8 @@
+import os
+
 from flask import Blueprint
+
+from .utils import get_config
 
 niviz_bp = Blueprint(
     'niviz_rater',
@@ -6,5 +10,17 @@ niviz_bp = Blueprint(
     template_folder='templates',
     url_prefix='/study/<string:study>/pipeline/<string:pipeline>'
 )
+
+
+def register_bp(app):
+    config = get_config()
+    if not config:
+        logger.error("Failed to register blueprint 'datman_niviz': "
+                     "Configuration not found.")
+        return
+    app.register_blueprint(niviz_bp)
+    app.config['NIVIZ_RATER_CONF'] = config
+    return app
+
 
 from . import views
